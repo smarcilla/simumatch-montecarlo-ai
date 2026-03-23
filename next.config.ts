2a7312477a9017d8230221360834dc37b2f5f25a
Sergio Marcilla <sermara70@gmail.com>
2026-03-23 13:52:02 +0100

    refactor: move internationalization support with locale resolution and message loading

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin(
+  "./src/infrastructure/ui/i18n/request.ts"
+);
 
 const nextConfig: NextConfig = {};
 
